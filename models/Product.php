8bf79dc2bb992cc8df7cdad0d29d35072903a2fc
Vladislav Valeev <soulproxy0404@gmail.com>
2020-02-25 21:01:30 +0300

    catalog and pager

--- models/Product.php
+++ models/Product.php
@@ -4,7 +4,7 @@
 class Product
 {
 
-    const SHOW_BY_DEFAULT = 10;
+    const SHOW_BY_DEFAULT = 6;
 
     /**
      * @return array of products
@@ -36,9 +36,60 @@ class Product
     }
 
 
-    public static function getProductsListByCategory($categoryId = false)
+    public static function getProductsListByCategory($categoryId = false, $page = 1)
     {
-        //START
+        if ($categoryId) {
+
+            $page = intval($page);
+            $offset = ($page - 1) * self::SHOW_BY_DEFAULT;
+
+            $db = Db::getConnection();
+            $products = array();
+            $result = $db->query("SELECT id, name, price, image, is_new FROM product "
+            . "WHERE status = '1' AND category_id = '$categoryId' "
+            . "ORDER BY id DESC "
+            . "LIMIT ".self::SHOW_BY_DEFAULT
+            . " OFFSET ". $offset);
+
+            $i = 0;
+            while ($row = $result->fetch()) {
+                $products[$i]['id'] = $row['id'];
+                $products[$i]['name'] = $row['name'];
+                $products[$i]['image'] = $row['image'];
+                $products[$i]['price'] = $row['price'];
+                $products[$i]['is_new'] = $row['is_new'];
+                $i++;
+            }
+
+            return $products;
+        }
+    }
+
+    public static function getProductById($id)
+    {
+        $id = intval($id);
+
+        if ($id) {
+            $db = Db::getConnection();
+
+            $result = $db->query('SELECT * FROM product WHERE id=' . $id);
+            $result->setFetchMode(PDO::FETCH_ASSOC);
+
+            return $result->fetch();
+        }
+    }
+
+
+    public static function getTotalProductsInCategory($categoryId)
+    {
+        $db = Db::getConnection();
+
+        $result = $db->query('SELECT count(id) AS count FROM product '
+            . 'WHERE status="1" AND category_id ="'.$categoryId.'"');
+        $result->setFetchMode(PDO::FETCH_ASSOC);
+        $row = $result->fetch();
+
+        return $row['count'];
     }
 
 }
